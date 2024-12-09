a2cc6e0a4484c5ec82dc6e8ecc7866b3f8be8132
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2024-12-09 09:34:31 -0300

    creación de listado albumes y enrutamiento (#2)

--- main.go
+++ main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type album struct {
+	ID     string  `json:"id"` // `json:"id"` es una etiqueta que se utiliza para serializar y deserializar objetos JSON
+	Title  string  `json:"title"` // `json:"title"` es una etiqueta que se utiliza para serializar y deserializar objetos JSON
+	Artist string  `json:"artist"` // `json:"artist"` es una etiqueta que se utiliza para serializar y deserializar objetos JSON
+	Price  float64 `json:"price"` // `json:"price"` es una etiqueta que se utiliza para serializar y deserializar objetos JSON
+}
+
+// Creamos una lista de álbumes
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+//Creamos un controlador
+func getAlbums(c *gin.Context) {
+	// la recomendación de golang es utilizar el paquete http para devolver respuestas HTTP
+	c.IndentedJSON(http.StatusOK, albums) // 200 es el código de estado HTTP que indica que la solicitud se ha completado con éxito
+}
+
 
 func main() {
 	fmt.Println("Bienvenido a la aplicación de ejemplo!")
 	// 01- creamos una nueva instancia de gin
 	router := gin.Default()
 
-	// 02- definimos una ruta
+	/* // 02- definimos una ruta
 	// Se le envía un mensaje en formato JSON, con un endpoint "/" y una función anónima que recibe un contexto
 	// y devuelve un mensaje en formato JSON
 	router.GET("/", func(c *gin.Context) {
@@ -21,5 +43,12 @@ func main() {
 	})
 
 	// 03- arrancamos el servidor en el puerto 8080
-	router.Run(":8080")
+	router.Run(":8080") */
+
+	// 04 - Definimos una ruta para obtener todos los álbumes
+	router.GET("/albums", getAlbums)
+
+	// 05 - arrancamos el servidor en el puerto 8080
+	router.Run(":3000")
+
 }
